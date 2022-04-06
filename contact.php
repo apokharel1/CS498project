69e6ac53f4e69856cf3360d52fdba2b2994c6f97
roshanshrestha009 <roshanshrestha007@gmail.com>
2022-04-06 17:42:24 -0400

    Contact / About us page of the School
    Further details of the school and ways to contact the school.

---
+++ contact.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="eng">
+
+<head>
+	<title>Learning Management System</title> 
+	<meta charset="utf-8"/>
+	
+	<link rel="stylesheet" href="style1.css" type="text/css"/>
+	<meta name="veiwport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body class="body">
+
+	<?php
+	include 'header1.php';
+	
+	?>
+	<br />
+	<br />
+	
+	<div class="maincontent">
+		<div class="content">
+			<article class="topcontent">
+				<header>
+					<h2><a href="#" title="first post">contacts</a></h2>
+				</header>
+				
+				
+				
+				
+				<content>
+					<p>	<h1>THIS PAGE IS UNDER CONSTRUCTION	</h1>
+					</p>
+				</content>
+			</article>	
+			
+			
+			
+		</div>
+	
+	</div>
+	
+	
+	
+	</body>
